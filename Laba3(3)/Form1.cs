0c6b6c623be84cfe4efd413dcee9c5cb1de398ba
Alex Vak <mamorunokitsune@gmail.com>
2019-03-13 23:46:28 +0200

    Laba3(3) 0.1

--- Laba3(3)/Form1.cs
+++ Laba3(3)/Form1.cs
@@ -16,5 +16,37 @@ namespace Laba3_3_
         {
             InitializeComponent();
         }
+        Pen pen;
+        Random rnd;
+        Random rndForColors; 
+        Bitmap myBitmap;
+        Graphics g;
+        private void DrawLine()
+        {
+            myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(myBitmap);
+            rnd = new Random();
+            rndForColors = new Random(DateTime.Now.Millisecond);
+            pen = new Pen(Color.FromArgb(rndForColors.Next(256), rndForColors.Next(256), rndForColors.Next(256), 10));
+            int x = rnd.Next(pictureBox1.Width);
+            int y = rnd.Next(pictureBox1.Height);
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x, y);
+            g.DrawLine(pen, point1, point2);
+            pictureBox1.Image = myBitmap;
+        }
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+        }
+
+
     }
 }
