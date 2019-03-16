0fa7cd4099f68cbf018f3b01eec8aa28172f2477
Alex Vak <mamorunokitsune@gmail.com>
2019-03-16 16:48:13 +0200

    Laba3(2) v1.0

--- Laba3(2)/Form1.cs
+++ Laba3(2)/Form1.cs
@@ -16,6 +16,7 @@ namespace Laba3_2_
         {
             InitializeComponent();
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -23,27 +24,38 @@ namespace Laba3_2_
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            Graphics g = pictureBox1.CreateGraphics();
+            Bitmap myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(myBitmap);
+            Font textFont = new Font("Stencil", 15);
 
             Rectangle rectBlue = new Rectangle(0, 100, 60, 60);
             Rectangle rectRed = new Rectangle(60, 100, 60, 60);
+            Rectangle rectDjrBlue = new Rectangle(0, 160, 120, 52);
             Rectangle rectForEllipse1 = new Rectangle(80, 120, 20, 20);
             Rectangle rectForEllipse2 = new Rectangle(5, 110, 20, 20);
             Rectangle rectForEllipse3 = new Rectangle(30, 130, 20, 20);
+
             SolidBrush sbBlue = new SolidBrush(Color.Blue);
             SolidBrush sbRed = new SolidBrush(Color.Red);
             SolidBrush sbWhite = new SolidBrush(Color.White);
+            SolidBrush sbDjrBlue = new SolidBrush(Color.DodgerBlue);
+
             g.RotateTransform(-40.0F);
             g.FillRectangle(sbBlue, rectBlue);
             g.FillRectangle(sbRed, rectRed);
+            g.FillRectangle(sbDjrBlue, rectDjrBlue);
             g.FillEllipse(sbWhite, rectForEllipse1);
             g.FillEllipse(sbWhite, rectForEllipse2);
             g.FillEllipse(sbWhite, rectForEllipse3);
+            g.DrawString("Domino\'s\n    Pizza", textFont, sbWhite, 12, 160);
 
-
-
-
-
+            pictureBox1.Image = myBitmap;
         }
+
+
+
     }
+
+
+ 
 }
