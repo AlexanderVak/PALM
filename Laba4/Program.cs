5239384126659421f2e97d4e5e68b7471566375a
Alex Vak <mamorunokitsune@gmail.com>
2019-03-20 23:49:04 +0200

    Laba4 v.0.1

--- Laba4/Program.cs
+++ Laba4/Program.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace Laba4
 {
+    struct MyFrac
+    {
+
+        public long nom, denom;
+        static long NSD(long n, long d)
+        {
+            while (n > 0 && d > 0)
+            {
+                if (n > d)
+                    n %= d;
+                else d %= n;
+            }
+            return n + d;
+        }
+        public MyFrac(long n, long d)
+        {
+
+
+
+            long res = NSD(n, d);
+            n /= res;
+            d /= res;
+
+            nom = n;
+            denom = d;
+        }
+        public void Output()
+        {
+            Console.WriteLine("{0} / {1}", nom, denom);
+        }
+    }
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            MyFrac output = new MyFrac(15, 40);
+            output.Output();
+            Console.ReadKey();
         }
     }
 }
