293bc5a28d4a74728f691aac7c79e1b278bd0f4b
Alex Vak <mamorunokitsune@gmail.com>
2019-04-18 02:24:46 +0300

    Laba4 v.1.0

--- Laba4/Program.cs
+++ Laba4/Program.cs
@@ -10,42 +10,71 @@ namespace Laba4
     {
 
         public long nom, denom;
-        static long NSD(long n, long d)
+        public MyFrac(long nom_, long denom_)
         {
-            while (n > 0 && d > 0)
+            nom_ = Math.Abs(nom_);
+            denom_ = Math.Abs(denom_);
+            nom = nom_;
+            denom = denom_;
+            while (nom_ > 0 && denom_ > 0)
             {
-                if (n > d)
-                    n %= d;
-                else d %= n;
+                if (nom_ > denom_)
+                    nom_ %= denom_;
+                else denom_ %= nom_;
             }
-            return n + d;
+            long res = nom_ + denom_;
+            nom /= res;
+            denom /= res;
+
         }
-        public MyFrac(long n, long d)
+        public override string ToString()
         {
-
-
-
-            long res = NSD(n, d);
-            n /= res;
-            d /= res;
-
-            nom = n;
-            denom = d;
-        }
-        public void Output()
-        {
-            Console.WriteLine("{0} / {1}", nom, denom);
+            string outputString = nom + "/" + denom;
+            return outputString;
         }
     }
+
     class Program
     {
+        static MyFrac Plus(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom + f1.denom * f2.nom, f1.denom * f2.denom);
+        }
 
+        static MyFrac Minus(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom - f1.denom * f2.nom, f1.denom * f2.denom);
+        }
+        
+        static double DoubleValue(MyFrac f)
+        {
+            return (double)f.nom / f.denom;
+        }
+
+        static MyFrac Multiply(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.nom, f1.denom * f2.denom);
+        }
+
+        static MyFrac Divide(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom, f1.denom * f2.nom);
+        }
 
         static void Main(string[] args)
         {
-            MyFrac output = new MyFrac(15, 40);
-            output.Output();
+            long nom_ = Convert.ToInt32(Console.ReadLine());
+            long denom_ = Convert.ToInt32(Console.ReadLine());
+            MyFrac myFrac = new MyFrac(nom_, denom_);
+            MyFrac myFrac1 = new MyFrac(15, 40);
+
+            Console.WriteLine("\nAddition of fractions\nResult : " + Plus(myFrac, myFrac1));
+            Console.WriteLine("\nSubtraction of fractions\nResult : " + Minus(myFrac, myFrac1));
+            Console.WriteLine("\nDouble value of fraction 1 is: {0}\nDouble value of fraction 1 is: {1}", DoubleValue(myFrac), DoubleValue(myFrac1));
+            Console.WriteLine("\nMultiplication of fractions\nResult : " + Multiply(myFrac, myFrac1));
+            Console.WriteLine("\nDivision of fractions\nResult : " + Divide(myFrac, myFrac1));
             Console.ReadKey();
         }
+
     }
 }
