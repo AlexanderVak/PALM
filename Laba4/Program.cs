3504cfa6283b88b6290aec0dbd934125a87c832c
Alex Vak <mamorunokitsune@gmail.com>
2019-04-25 03:49:08 +0300

    Laba4 v.2.0

--- Laba4/Program.cs
+++ Laba4/Program.cs
@@ -61,18 +61,42 @@ namespace Laba4
             return new MyFrac(f1.nom * f2.denom, f1.denom * f2.nom);
         }
 
+        static MyFrac GetRGR113LeftSum(int n)
+        {
+            MyFrac res = new MyFrac(1, 1);
+            for (int i = 1; i <= n ; i+=2)
+            {
+          //      res = new MyFrac(n, Multiply(new MyFrac())
+            }
+            res = Minus(res, new MyFrac(1, 1));
+            return res;
+        }
+
+        static MyFrac GetRGR115LeftSum(int n)
+        {
+            MyFrac res = new MyFrac(1, 1);
+            for (int i = 2; i <= n; i++)
+            {
+                res = Multiply(res, Minus(new MyFrac(1, 1), new MyFrac(1, i * i)));
+            }
+            return res;
+        }
+
         static void Main(string[] args)
         {
             long nom_ = Convert.ToInt32(Console.ReadLine());
             long denom_ = Convert.ToInt32(Console.ReadLine());
             MyFrac myFrac = new MyFrac(nom_, denom_);
             MyFrac myFrac1 = new MyFrac(15, 40);
+            int n = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("\nAddition of fractions\nResult : " + Plus(myFrac, myFrac1));
             Console.WriteLine("\nSubtraction of fractions\nResult : " + Minus(myFrac, myFrac1));
             Console.WriteLine("\nDouble value of fraction 1 is: {0}\nDouble value of fraction 1 is: {1}", DoubleValue(myFrac), DoubleValue(myFrac1));
             Console.WriteLine("\nMultiplication of fractions\nResult : " + Multiply(myFrac, myFrac1));
             Console.WriteLine("\nDivision of fractions\nResult : " + Divide(myFrac, myFrac1));
+            Console.WriteLine("\nGetRGR113LeftSum\nResult : " + GetRGR113LeftSum(n));
+            Console.WriteLine("\nGetRGR115LeftSum\nResult : " + GetRGR115LeftSum(n));
             Console.ReadKey();
         }
 
